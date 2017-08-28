72e33678e9dc6ed4ff30f532b2263b8865b38457
Manuel Tanzi <manuel.tanzi@okta.com>
2017-08-27 18:50:01 -0700

    test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,6 +6,7 @@ setup_service grunt
 setup_service bundler
 
 # Use newer, faster yarn
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
 yes | yum install yarn
 
 # Install required dependencies
