f593e3611c9db89471b8282c4ae082f468d7abbf
kelvinzhu-okta <kelvin.zhu@okta.com>
2018-10-12 14:14:52 -0700

    Removing cafile set.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,8 +6,6 @@ setup_service grunt
 setup_service bundler
 setup_service yarn 1.7.0
 
-yarn config set cafile /root/Okta-Root-CA.pem
-
 # Install required dependencies
 yarn global add  @okta/ci-update-package
 yarn global add @okta/ci-pkginfo
