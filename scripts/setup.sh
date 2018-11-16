96084204a211274996eb455abee99d8c3c5cbd99
Manuel Tanzi <manuel.tanzi@okta.com>
2018-11-16 12:08:54 -0800

    :art: Deprecates scss-lint and removes ruby dependencies
    Resolves: OKTA-196548

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,17 +2,9 @@
 
 cd ${OKTA_HOME}/${REPO}
 
-setup_service grunt
-setup_service bundler
-
 # Install required dependencies
 npm install -g @okta/ci-update-package
 npm install -g @okta/ci-pkginfo
-
-if ! bundle install; then
-  echo "bundle install failed! Exiting..."
-  exit ${FAILED_SETUP}
-fi
 
 if ! npm install --no-optional --unsafe-perm; then
   echo "npm install failed! Exiting..."
