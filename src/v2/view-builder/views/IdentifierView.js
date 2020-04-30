5366da06efaefa59470a8ea91b8fad2e9d10a725
Jintai He <jhe@okta.com>
2020-04-30 14:13:41 -0700

    feat: test reducing universal link clicks     OKTA-289825

--- src/v2/view-builder/views/IdentifierView.js
+++ src/v2/view-builder/views/IdentifierView.js
@@ -1,7 +1,9 @@
-import { View, loc, createButton } from 'okta';
+import { $, View, loc, createButton } from 'okta';
 import BaseView from '../internals/BaseView';
 import BaseForm from '../internals/BaseForm';
 import BaseFooter from '../internals/BaseFooter';
+import { fetchRequest } from '../../ion/httpClient';
+import Util from '../../../util/Util';
 
 const Body = BaseForm.extend({
 
@@ -17,12 +19,38 @@ const Body = BaseForm.extend({
           <div class="separation-line"><span>OR</span></div>
         `,
         initialize () {
-          const appState = this.options.appState;
+          // const appState = this.options.appState;
           this.add(createButton({
             className: 'button-secondary',
             title: 'Sign in using Okta Verify',
             click () { 
-              appState.trigger('invokeAction', 'launch-authenticator');
+              // appState.trigger('invokeAction', 'launch-authenticator');
+              const rem = this.options.appState.get('rawIdxState').remediation.value
+              .filter(v => v.name === 'launch-authenticator')[0];
+              // fetchRequest(
+              //   rem.href,
+              //   rem.method,
+              //   { stateHandle: rem.value[0].value }
+              // )
+              // .then((resp) => {
+              //   const response = resp.response;
+              //   const deviceChallenge = response[
+              //     resp.response.remediation.value.filter(v => v.name === 'device-challenge-poll')[0].relatesTo
+              //   ];
+              //   Util.redirect(deviceChallenge.value.href);
+              // });
+              $.ajax({
+                url: rem.href,
+                method: rem.method,
+                contentType: 'application/json',
+                data: JSON.stringify({ stateHandle: rem.value[0].value }),
+              })
+              .then((resp) => {
+                const deviceChallenge = resp[
+                  resp.remediation.value.filter(v => v.name === 'device-challenge-poll')[0].relatesTo
+                ];
+                Util.redirect(deviceChallenge.value.href);
+              });
             }
           }), '.okta-verify-container');
         }
